9b757e9d80e023ddc02eb4017266522c5b4f84e9
Dylan <thedmp8@gmail.com>
2026-05-02 14:23:33 +1000

    Running site Initial

--- ANUPlanner/Program.cs
+++ ANUPlanner/Program.cs
@@ -6,6 +6,13 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<CsvDataService>();
 
+// Configure Kestrel to allow immediate port reuse
+builder.WebHost.UseKestrel(options =>
+{
+    options.ListenLocalhost(5000);
+    options.ListenLocalhost(5001, listenOptions => listenOptions.UseHttps());
+});
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -14,10 +21,19 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
-app.Run();
+// Ensure graceful shutdown
+var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
+lifetime.ApplicationStopping.Register(async () =>
+{
+    Console.WriteLine("Application is shutting down...");
+    await Task.Delay(100); // Allow final cleanup
+});
+
+await app.RunAsync();
